0f963df31811856129b9370c1a2851ad39d81a1b
David-Vau <githup46.parsonal678@gmail.com>
2024-08-31 19:24:53 +0600

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,5 +1,4 @@
 import os,time,platform
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 import fire
-elif bit=='32bit':
-import fire ()
+fire import()
