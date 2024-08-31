388882ec0af9fa035ef061ce87b2178e61880a99
David-Vau <githup46.parsonal678@gmail.com>
2024-08-31 19:26:57 +0600

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,4 +1,4 @@
 import os,time,platform
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 import fire
-fire.import()
+fire.Brand()
